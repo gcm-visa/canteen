eb78da60adfd26657dcdd346def80c428118336d
gcm-visa <visa@gcm.edu.ph>
2025-11-29 22:02:16 +0800

    Implement caching in service worker
    Added caching functionality to the service worker.

--- sw.js
+++ sw.js
@@ -1,11 +1,22 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
+const CACHE_NAME = 'chilleat-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/chilleat-logo.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activated');
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
