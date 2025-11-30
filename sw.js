4cd7868525d3ddc53e7dc5fb3529b8b9b28ab61c
gcm-visa <visa@gcm.edu.ph>
2025-11-30 12:41:52 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,30 @@
-self.addEventListener("install", () => {});
-self.addEventListener("activate", () => {});
+let deferredPrompt;
+const modal = document.getElementById("pwaModal");
+const installBtn = document.getElementById("installBtn");
+const closeModal = document.getElementById("closeModal");
+
+// Wait for Install Prompt
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  // show install modal
+  modal.style.display = "flex";
+});
+
+// Install button clicked
+installBtn.addEventListener("click", async () => {
+  modal.style.display = "none";
+
+  if (!deferredPrompt) return;
+
+  deferredPrompt.prompt();
+  const choice = await deferredPrompt.userChoice;
+
+  deferredPrompt = null;
+});
+
+// close modal
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
